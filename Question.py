70289a9f01ece45d184dcee142bc870ec38d6a2a
skywalker007-cpu <656749873@qq.com>
2024-11-18 18:43:48 -0800

    question.py for quesiton class and its subclass

---
+++ Question.py
@@ -0,0 +1,69 @@
+from collections import UserDict
+
+
+class Question(UserDict):
+    """
+    Question class to store question data within a nested dictionary format. We extend the UserDict class to
+    allow us to use the data attribute to access the nested dictionary.
+    :param **kwargs: is used to make class creation work with dictionary function.
+    """
+
+    def __init__(self, question_id, question_text, correct_answer, score=1, **kwargs):
+        super().__init__()
+        self.data["Q_id"] = question_id
+        self.data["question"] = question_text
+        self.data["answer"] = correct_answer
+        self.data["score"] = score
+        # update key word pairs for the subclass that have additional attribute.
+        self.data.update(kwargs)
+
+    def __str__(self):
+        return f"ID: {self.data['id']}, Question: {self.data['question']}, Score: {self.data['score']}"
+
+
+class MultipleChoice(Question):
+    """
+    Multiple choice class to store multiple choice question data.
+    """
+
+    def __init__(self, question_id, question_text, correct_answer, score=1, options=None, **kwargs):
+        super().__init__(question_id, question_text, correct_answer, score)
+        if options is None:
+            options = []
+        self.data["options"] = options
+
+    def __str__(self):
+        for option in self.data["options"]:
+            return f"{super().__str__()}\nOptions:\n{option}"
+
+
+class ShortAnswerQuestion(Question):
+    """
+    Short answer question class to store short answer question data.
+    """
+
+    def __init__(self, question_id, question_text, correct_answer, score=1, notice=None, **kwargs):
+        super().__init__(question_id, question_text, correct_answer, score)
+        # notice attribute can be something like "please answer in one sentence"
+        if notice is None:
+            notice = ""
+        self.data["notice"] = notice
+
+    def __str__(self):
+        notification = self.data["notice"]
+        return f"{super().__str__()}\nOptions:\n{notification}"
+
+
+class TrueFalseQuestion(Question):
+    """
+    True false question class to store true false question data.
+    """
+
+    def __init__(self, question_id, question_text, correct_answer, score=1, options=None, **kwargs):
+        super().__init__(question_id, question_text, correct_answer, score)
+        if options is None:
+            options = ["True", "False"]
+        self.data["options"] = options
+
+    def __str__(self):
+        return f"{super().__str__()}\nOptions:\nTrue\nFalse"
