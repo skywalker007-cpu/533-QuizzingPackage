ddfac854a0edaa1c317331ab4938ede6f0b3244b
Haoxiang Xu <haoxiang-xu>
2024-12-05 12:05:37 -0800

    quiz tests added

--- quiz_test.py
+++ quiz_test.py
@@ -4,6 +4,7 @@ import Objects.question as question
 from Objects.response import Response
 
 class TestQuiz(unittest.TestCase):
+    
     @classmethod
     def setUpClass(cls):
         cls.Q1 = question.MultipleChoice(1, "What is the capital of France?", "Paris", 1,
@@ -14,6 +15,17 @@ class TestQuiz(unittest.TestCase):
                                         ["Paris", "London", "Berlin", "Madrid"])
         cls.Q5 = question.ShortAnswerQuestion(5, "What is the capital of Germany?", "Berlin", 1, "Please answer in one word")
         cls.Q6 = question.TrueFalseQuestion(6, "Is Berlin the capital of Germany?", "True", 1, ["True", "False"])
+    
+        cls.correct_response_1 = Response("001", ["Paris", "Paris", "True"])
+        cls.correct_response_2 = Response("002", ["Paris", "Paris", "True"])
+        cls.correct_response_3 = Response("003", ["Paris", "Paris", "True"])
+        cls.incorrect_response_1 = Response("004", ["Paris", "Paris", "False"])
+        cls.incorrect_response_2 = Response("005", ["Paris", "Paris", "False"])
+        cls.incorrect_response_3 = Response("006", ["Paris", "Paris", "False"])
+    
+    @classmethod
+    def tearDownClass(cls):
+        return super().tearDownClass()
         
     def setUp(self):
         self.quiz_1 = Quiz()
@@ -30,14 +42,10 @@ class TestQuiz(unittest.TestCase):
         
         self.quiz_3 = self.quiz_1.clone()
         self.quiz_for_editing = Quiz()
-        
-        self.correct_response_1 = Response("001", ["Paris", "Paris", "True"])
-        self.correct_response_2 = Response("002", ["Paris", "Paris", "True"])
-        self.correct_response_3 = Response("003", ["Paris", "Paris", "True"])
-        self.incorrect_response_1 = Response("004", ["Paris", "Paris", "False"])
-        self.incorrect_response_2 = Response("005", ["Paris", "Paris", "False"])
-        self.incorrect_response_3 = Response("006", ["Paris", "Paris", "False"])
-        
+             
+    def tearDown(self) -> None:
+        return super().tearDown()
+
     def test_clone(self):
         self.assertEqual(self.quiz_1, self.quiz_3)
         self.assertNotEqual(self.quiz_1, self.quiz_2)
@@ -76,8 +84,31 @@ class TestQuiz(unittest.TestCase):
         self.quiz_1.add_responses({"002" : Response("002", ["Paris", "Paris", "True"])})
         self.quiz_1.add_responses({"003" : Response("003", ["Paris", "Paris", "True"])})
         self.assertEqual(3, len(self.quiz_1.responses))
-       
-    def test(self):
-        return 
-
+        
+        self.assertEqual(0, len(self.quiz_2.responses))
+        
+        self.quiz_2.add_responses({"004" : Response("004", ["Paris", "Paris", "False"])})
+        self.quiz_2.add_responses({"005" : Response("005", ["Paris", "Paris", "False"])})
+        self.quiz_2.add_responses({"005" : Response("005", ["Paris", "Paris", "False"])})
+        
+        self.assertEqual(2, len(self.quiz_2.responses))
+        self.assertNotEqual(3, len(self.quiz_2.responses))
+        
+    def test_marking(self):
+        self.quiz_1.add_responses({"001" : Response("001", ["Paris", "Paris", "True"])})
+        self.quiz_1.add_responses({"002" : Response("002", ["Paris", "", "False"])})
+        self.quiz_1.add_responses({"003" : Response("003", ["Paris", "Paris", "False"])})
+        self.quiz_1.add_responses({"004" : Response("004", ["", "", "False"])})
+        
+        self.quiz_1.mark_quiz()
+        student_1_score = self.quiz_1.marks["001"]
+        student_2_score = self.quiz_1.marks["002"]
+        student_3_score = self.quiz_1.marks["003"]
+        student_4_score = self.quiz_1.marks["004"]
+        
+        self.assertEqual(3, student_1_score)
+        self.assertEqual(1, student_2_score)
+        self.assertEqual(2, student_3_score)
+        self.assertEqual(0, student_4_score)
+    
 unittest.main(argv=[''], exit=False)
